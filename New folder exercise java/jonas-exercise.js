d97adb5d06698bb67dbfd07ed0d951494d3322be
fowobi <109865505+fowobi@users.noreply.github.com>
2023-03-28 22:09:08 +0100

    updated

---
+++ New folder exercise java/jonas-exercise.js
@@ -0,0 +1,19 @@
+//let massJohn =92;
+//let heightJohn =1.95;
+//let massMark=78;
+//let heightMark= 1.69;
+
+//let bodyMassIndexJohn = massJohn / (heightJohn * heightJohn);
+//let bodyMassIndexMark = massMark / (heightMark * heightMark);
+//console.log(bodyMassIndexJohn,bodyMassIndexMark);
+//console.log (bodyMassIndexMark > bodyMassIndexJohn);
+
+let massJohn=85;
+let heightJohn=1.76;
+let massMark=95;
+let heightMark=1.88;
+let bodyMassIndexJohn = massJohn / (heightJohn * heightJohn);
+let bodyMassIndexMark = massMark / (heightMark * heightMark);
+console.log(bodyMassIndexJohn,bodyMassIndexMark);
+console.log (bodyMassIndexMark > bodyMassIndexJohn);
+
