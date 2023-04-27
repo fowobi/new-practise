5b277f292df4343248bd839e50ee4ef214618f29
fowobi <109865505+fowobi@users.noreply.github.com>
2023-04-27 20:04:30 +0100

    updated

--- New folder exercise java/jonas-exercise.js
+++ New folder exercise java/jonas-exercise.js
@@ -1,19 +1,27 @@
-//let massJohn =92;
-//let heightJohn =1.95;
-//let massMark=78;
-//let heightMark= 1.69;
+let massJohn =92;
+let heightJohn =1.95;
+let massMark=78;
+let heightMark= 1.69;
 
-//let bodyMassIndexJohn = massJohn / (heightJohn * heightJohn);
-//let bodyMassIndexMark = massMark / (heightMark * heightMark);
-//console.log(bodyMassIndexJohn,bodyMassIndexMark);
-//console.log (bodyMassIndexMark > bodyMassIndexJohn);
-
-let massJohn=85;
-let heightJohn=1.76;
-let massMark=95;
-let heightMark=1.88;
 let bodyMassIndexJohn = massJohn / (heightJohn * heightJohn);
 let bodyMassIndexMark = massMark / (heightMark * heightMark);
 console.log(bodyMassIndexJohn,bodyMassIndexMark);
 console.log (bodyMassIndexMark > bodyMassIndexJohn);
+
+//you can use this when you want another sentence in another line
+console.log(`String
+multiple
+lines`);
+//OR this
+console.log('String with \n\ mutiple \n\ lines');
+
+
+// let massJohn=85;
+// let heightJohn=1.76;
+// let massMark=95;
+// let heightMark=1.88;
+// let bodyMassIndexJohn = massJohn / (heightJohn * heightJohn);
+// let bodyMassIndexMark = massMark / (heightMark * heightMark);
+// console.log(bodyMassIndexJohn,bodyMassIndexMark);
+// console.log (bodyMassIndexMark > bodyMassIndexJohn);
 
